735b8b49f08be26e8a34e455efd11d0c2e418592
Lyon Sun <sunly917@gmail.com>
2015-12-11 12:03:57 +0200

    add product table view cell class.

---
+++ GuanJia/ProductTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductTableViewCell.swift
+//  GuanJia
+//
+//  Created by Liang Sun on 12/11/15.
+//  Copyright © 2015 Lyon Sun. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
